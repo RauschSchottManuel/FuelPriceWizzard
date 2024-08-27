ab0dda3089d6d22fbb23d57c2d1393643c92724a
Manu <manuel.rausch.schott@gmail.com>
2024-08-27 19:22:53 +0200

    Add basic project structure

---
+++ FuelPriceWizzard.BusinessLogic/Implementation/FuelPriceWizardService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelPriceWizard.BusinessLogic.Implementation
+{
+    public class FuelPriceWizardService : IFuelPriceWizardService
+    {
+    }
+}
---
+++ FuelPriceWizzard.BusinessLogic/ServiceRegistrationHelper.cs
@@ -0,0 +1,15 @@
+﻿using FuelPriceWizard.BusinessLogic.Implementation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FuelPriceWizard.BusinessLogic
+{
+    public static class ServiceRegistrationHelper
+    {
+        public static IServiceCollection AddFuelPriceWizardBusinessLogic(this IServiceCollection services)
+        {
+            services.AddScoped<IFuelPriceWizardService, FuelPriceWizardService>();
+
+            return services;
+        }
+    }
+}
