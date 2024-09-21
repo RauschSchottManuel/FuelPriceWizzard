2376728593ec71a13b8be2d858793343f53f05df
Manu <manuel.rausch.schott@gmail.com>
2024-09-21 16:32:26 +0200

    Fix angular unit tests

--- FuelPriceWizard.UI/fuelpricewizard/src/app/shared/osm-map/osm-map.component.spec.ts
+++ FuelPriceWizard.UI/fuelpricewizard/src/app/shared/osm-map/osm-map.component.spec.ts
@@ -1,9 +1,11 @@
+import { ComponentRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OsmMapComponent } from './osm-map.component';
 
 describe('OsmMapComponent', () => {
   let component: OsmMapComponent;
+  let componentRef: ComponentRef<OsmMapComponent>;
   let fixture: ComponentFixture<OsmMapComponent>;
 
   beforeEach(async () => {
@@ -14,6 +16,11 @@ describe('OsmMapComponent', () => {
 
     fixture = TestBed.createComponent(OsmMapComponent);
     component = fixture.componentInstance;
+    componentRef = fixture.componentRef;
+
+    componentRef.setInput("width", "100");
+    componentRef.setInput("height", "1ß0");
+
     fixture.detectChanges();
   });
 
