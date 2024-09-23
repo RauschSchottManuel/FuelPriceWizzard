1612bf4c8559a8e436d427935b734d31ae9c2ecd
Manu <manuel.rausch.schott@gmail.com>
2024-09-23 20:39:17 +0200

    Add simple documentation for IFuelPriceSourceService

--- FuelPriceWizard.BusinessLogic/IFuelPriceSourceService.cs
+++ FuelPriceWizard.BusinessLogic/IFuelPriceSourceService.cs
@@ -3,8 +3,15 @@ using Microsoft.Extensions.Configuration;
 
 namespace FuelPriceWizard.BusinessLogic
 {
+    /// <summary>
+    /// <c>IFuelPriceSourceService</c> defines the interface implemented by collector service classes
+    /// </summary>
     public interface IFuelPriceSourceService
     {
+        /// <summary>
+        /// The method <c>GetFetchSettingsSection</c> returns the specific <c>FetchSettings</c> configuration section 
+        /// </summary>
+        /// <returns>An object of type <see cref="IConfigurationSection"/></returns>
         IConfigurationSection GetFetchSettingsSection();
         object FetchPricesByLocation(int lat, int lon);
         object FetchPricesByLocationAndFuelType(int lat, int lon, FuelType fuelType, int maxReturnResults = 1);
