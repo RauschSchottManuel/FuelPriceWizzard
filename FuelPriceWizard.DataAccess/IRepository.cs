b556b77af9660bc9c8cf7dc38c558aad98b9329d
Simon Rausch-Schott <simon.rauschschott@gmail.com>
2024-10-18 20:54:15 +0200

    Implement CRUD methods for IRepository

--- FuelPriceWizard.DataAccess/IRepository.cs
+++ FuelPriceWizard.DataAccess/IRepository.cs
@@ -3,6 +3,13 @@
     public interface IRepository<TDomainModel>
     {
         public Task<IEnumerable<TDomainModel>> GetAllAsync();
-        //TODO: CRUD
+        public Task<TDomainModel?> GetByIdAsync(int id);
+
+        public Task<bool> DeleteAsync(TDomainModel model);
+        public Task<bool> DeleteByIdAsync(int id);
+
+        public Task<TDomainModel> UpdateAsync(TDomainModel model);
+
+        public Task<TDomainModel> InsertAsync(TDomainModel model);
     }
 }
--- FuelPriceWizard.DataAccess/Implementation/BaseRepository.cs
+++ FuelPriceWizard.DataAccess/Implementation/BaseRepository.cs
@@ -1,9 +1,13 @@
 ﻿using AutoMapper;
+using FuelPriceWizard.DataAccess.Entities;
+using FuelPriceWizard.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace FuelPriceWizard.DataAccess.Implementation
 {
-    public class BaseRepository<TDataModel, TDomainModel> : IRepository<TDomainModel> where TDataModel : class
+    public class BaseRepository<TDataModel, TDomainModel> : IRepository<TDomainModel> 
+        where TDataModel : BaseEntity 
+        where TDomainModel : BaseModel
     {
         public FuelPriceWizardDbContext Context { get; set; }
         public IMapper Mapper { get; set; }
@@ -20,6 +24,49 @@ namespace FuelPriceWizard.DataAccess.Implementation
             var entities = await this.Context.Set<TDataModel>().ToListAsync();
 
             return this.Mapper.Map<IEnumerable<TDomainModel>>(entities);
+        }
+
+        public async Task<TDomainModel?> GetByIdAsync(int id)
+        {
+            var entity = await this.Context.Set<TDataModel>().SingleOrDefaultAsync(e => e.Id == id);
+
+            return this.Mapper.Map<TDomainModel>(entity);
+        }
+
+        public async Task<bool> DeleteAsync(TDomainModel model)
+        {
+            return await DeleteByIdAsync(model.Id);
+        }
+
+        public async Task<bool> DeleteByIdAsync(int id)
+        {
+            var rowsAffected = await this.Context.Set<TDataModel>()
+                .Where(e => e.Id == id)
+                .ExecuteDeleteAsync();
+
+            await this.Context.SaveChangesAsync();
+            
+            return rowsAffected > 0;
+        }
+
+        public async Task<TDomainModel> UpdateAsync(TDomainModel model)
+        {
+            var entity = this.Mapper.Map<TDataModel>(model);
+
+            var result = this.Context.Update(entity);
+            await this.Context.SaveChangesAsync();
+
+            return Mapper.Map<TDomainModel>(result);
+        }
+
+        public async Task<TDomainModel> InsertAsync(TDomainModel model)
+        {
+            var entity = this.Mapper.Map<TDataModel>(model);
+
+            var result = this.Context.Set<TDataModel>().Add(entity);
+            await this.Context.SaveChangesAsync();
+
+            return Mapper.Map<TDomainModel>(result);
         }
     }
 }
